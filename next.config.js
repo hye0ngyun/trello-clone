dcc512dbfc1868c67cb788f00daa62997b0f6ee4
Hyeongyun <shg7271@gmail.com>
2023-11-29 23:13:06 +0900

    Update: README.md and deploy script - npm run deploy

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
    */
-  // output: "export",
+  output: "export",
   /**
    * build output directory name
    */
@@ -17,7 +17,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  // basePath: "/",
+  basePath: "/trello-clone",
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
