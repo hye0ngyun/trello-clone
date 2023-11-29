e10357a5f6a8cd0b0dfde23eb6495cd886455953
Hyeongyun <shg7271@gmail.com>
2023-11-29 21:40:46 +0900

    Build&Deploy

--- src/app/drag-drop/page.tsx
+++ src/app/drag-drop/page.tsx
@@ -1,118 +1,121 @@
-"use client";
-import Board from "@/lib/components/Board";
-import Modal from "@/lib/components/Modal";
-import { draggingBoardState, toDoState } from "@/lib/store/drag-drop";
-import {
-  DragDropContext,
-  DragStart,
-  Draggable,
-  DropResult,
-  Droppable,
-} from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
-import styled from "styled-components";
-
 export default function DragDrop() {
-  const [toDo, setToDo] = useRecoilState(toDoState);
-  const [draggingBoard, setdraggingBoard] = useRecoilState(draggingBoardState);
-  const onDragEnd = (info: DropResult): void => {
-    if (!info.destination) return;
-    const { draggableId, destination, source } = info;
-    // 보드 욺기는 경우
-    if (source.droppableId === "all") {
-      const arr = Object.keys(toDo);
-      arr.splice(source.index, 1);
-      arr.splice(destination.index, 0, draggableId.replace("board-", ""));
-      setToDo((prev) => {
-        const temp: { [key: string]: string[] } = {};
-        for (let i = 0; i < arr.length; i++) {
-          temp[arr[i]] = prev[arr[i]];
-        }
-        return temp;
-      });
-      return;
-    }
-    // 보드 내의 아이템 욺기는 경우
-    if (destination.droppableId === source.droppableId) {
-      // 같은 보드에서 이동이 됐다면
-      setToDo((allBoards) => {
-        const temp = [...allBoards[source.droppableId]];
-        // 1. 드래그 시작한 값 삭제
-        temp.splice(source.index, 1);
-        // 2. 드래그 끝나는 위치에 시작한 값 추가
-        temp.splice(destination?.index, 0, draggableId);
-        return {
-          ...allBoards,
-          [source.droppableId]: temp,
-        };
-      });
-    } else {
-      // 다른 보드로 이동이 됐다면
-      setToDo((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        // 1. 드래그 시작한 값 삭제
-        sourceBoard.splice(source.index, 1);
-        // 2. 드래그 끝나는 위치에 시작한 값 추가
-        destinationBoard.splice(destination?.index, 0, draggableId);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
-  const onDragStart = (info: DragStart): void => {
-    setdraggingBoard(info.source.droppableId);
-  };
-  return (
-    <Wrapper>
-      <Modal>
-        <Modal.Title>title</Modal.Title>
-        <Modal.Content>content</Modal.Content>
-      </Modal>
-      <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
-        <Droppable
-          direction="horizontal"
-          droppableId="all"
-          isDropDisabled={draggingBoard !== "all"}
-        >
-          {(magic) => (
-            <Boards ref={magic.innerRef} {...magic.droppableProps}>
-              {Object.keys(toDo).map((key, index) => (
-                <Draggable
-                  draggableId={`board-${key}`}
-                  index={index}
-                  key={`board-${key}`}
-                >
-                  {(magic) => (
-                    <Board
-                      draggableProvider={magic}
-                      todos={toDo[key]}
-                      droppableId={key}
-                    />
-                  )}
-                </Draggable>
-              ))}
-              {magic.placeholder}
-            </Boards>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </Wrapper>
-  );
+  return <></>;
 }
+// "use client";
+// import Board from "@/lib/components/Board";
+// import Modal from "@/lib/components/Modal";
+// import { draggingBoardState, toDoState } from "@/lib/store/drag-drop";
+// import {
+//   DragDropContext,
+//   DragStart,
+//   Draggable,
+//   DropResult,
+//   Droppable,
+// } from "react-beautiful-dnd";
+// import { useRecoilState } from "recoil";
+// import styled from "styled-components";
 
-const Wrapper = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  min-height: 100vh;
-`;
-const Boards = styled.div`
-  display: flex;
-  gap: 10px;
-`;
+// export default function DragDrop() {
+//   const [toDo, setToDo] = useRecoilState(toDoState);
+//   const [draggingBoard, setdraggingBoard] = useRecoilState(draggingBoardState);
+//   const onDragEnd = (info: DropResult): void => {
+//     if (!info.destination) return;
+//     const { draggableId, destination, source } = info;
+//     // 보드 욺기는 경우
+//     if (source.droppableId === "all") {
+//       const arr = Object.keys(toDo);
+//       arr.splice(source.index, 1);
+//       arr.splice(destination.index, 0, draggableId.replace("board-", ""));
+//       setToDo((prev) => {
+//         const temp: { [key: string]: string[] } = {};
+//         for (let i = 0; i < arr.length; i++) {
+//           temp[arr[i]] = prev[arr[i]];
+//         }
+//         return temp;
+//       });
+//       return;
+//     }
+//     // 보드 내의 아이템 욺기는 경우
+//     if (destination.droppableId === source.droppableId) {
+//       // 같은 보드에서 이동이 됐다면
+//       setToDo((allBoards) => {
+//         const temp = [...allBoards[source.droppableId]];
+//         // 1. 드래그 시작한 값 삭제
+//         temp.splice(source.index, 1);
+//         // 2. 드래그 끝나는 위치에 시작한 값 추가
+//         temp.splice(destination?.index, 0, draggableId);
+//         return {
+//           ...allBoards,
+//           [source.droppableId]: temp,
+//         };
+//       });
+//     } else {
+//       // 다른 보드로 이동이 됐다면
+//       setToDo((allBoards) => {
+//         const sourceBoard = [...allBoards[source.droppableId]];
+//         const destinationBoard = [...allBoards[destination.droppableId]];
+//         // 1. 드래그 시작한 값 삭제
+//         sourceBoard.splice(source.index, 1);
+//         // 2. 드래그 끝나는 위치에 시작한 값 추가
+//         destinationBoard.splice(destination?.index, 0, draggableId);
+//         return {
+//           ...allBoards,
+//           [source.droppableId]: sourceBoard,
+//           [destination.droppableId]: destinationBoard,
+//         };
+//       });
+//     }
+//   };
+//   const onDragStart = (info: DragStart): void => {
+//     setdraggingBoard(info.source.droppableId);
+//   };
+//   return (
+//     <Wrapper>
+//       <Modal>
+//         <Modal.Title>title</Modal.Title>
+//         <Modal.Content>content</Modal.Content>
+//       </Modal>
+//       <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
+//         <Droppable
+//           direction="horizontal"
+//           droppableId="all"
+//           isDropDisabled={draggingBoard !== "all"}
+//         >
+//           {(magic) => (
+//             <Boards ref={magic.innerRef} {...magic.droppableProps}>
+//               {Object.keys(toDo).map((key, index) => (
+//                 <Draggable
+//                   draggableId={`board-${key}`}
+//                   index={index}
+//                   key={`board-${key}`}
+//                 >
+//                   {(magic) => (
+//                     <Board
+//                       draggableProvider={magic}
+//                       todos={toDo[key]}
+//                       droppableId={key}
+//                     />
+//                   )}
+//                 </Draggable>
+//               ))}
+//               {magic.placeholder}
+//             </Boards>
+//           )}
+//         </Droppable>
+//       </DragDropContext>
+//     </Wrapper>
+//   );
+// }
+
+// const Wrapper = styled.div`
+//   background-color: ${(props) => props.theme.bgColor};
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   width: 100%;
+//   min-height: 100vh;
+// `;
+// const Boards = styled.div`
+//   display: flex;
+//   gap: 10px;
+// `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,140 @@
 "use client";
-import { hourSeletor, minuteState, secondSeletor } from "@/lib/store/atom";
-import { useRecoilState, useRecoilValue } from "recoil";
+import Board from "@/lib/components/Board";
+import { ITodo, draggingBoardState, toDoState } from "@/lib/store/drag-drop";
+import { useState, useEffect } from "react";
+import {
+  DragDropContext,
+  DragStart,
+  Draggable,
+  DropResult,
+  Droppable,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
 
-type TInputEvent = React.FormEvent<HTMLInputElement>;
-export default function Home() {
-  const [minute, setMinute] = useRecoilState(minuteState);
-  const [hour, setHour] = useRecoilState(hourSeletor);
-  const [second, setSecond] = useRecoilState(secondSeletor);
-  const onMinuteChange = (event: TInputEvent) => {
-    setMinute(+event.currentTarget.value);
+export default function DragDrop() {
+  const [toDo, setToDo] = useRecoilState(toDoState);
+  const [draggingBoard, setdraggingBoard] = useRecoilState(draggingBoardState);
+  const onDragEnd = (info: DropResult): void => {
+    if (!info.destination) return;
+    const { draggableId, destination, source } = info;
+    // 보드 욺기는 경우
+    if (source.droppableId === "all") {
+      const arr = Object.keys(toDo);
+      arr.splice(source.index, 1);
+      arr.splice(destination.index, 0, draggableId.replace("board-", ""));
+      setToDo((prev) => {
+        const temp: { [key: string]: ITodo[] } = {};
+        for (let i = 0; i < arr.length; i++) {
+          temp[arr[i]] = prev[arr[i]];
+        }
+        return temp;
+      });
+      return;
+    }
+    // 보드 내의 아이템 욺기는 경우
+    if (destination.droppableId === source.droppableId) {
+      // 같은 보드에서 이동이 됐다면
+      setToDo((allBoards) => {
+        const temp = [...allBoards[source.droppableId]];
+        // 1. 드래그 시작한 값 삭제
+        const sourceValue = temp.splice(source.index, 1)[0];
+        // 2. 드래그 끝나는 위치에 시작한 값 추가
+        temp.splice(destination?.index, 0, sourceValue);
+        return {
+          ...allBoards,
+          [source.droppableId]: temp,
+        };
+      });
+    } else {
+      // 다른 보드로 이동이 됐다면
+      setToDo((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        // 1. 드래그 시작한 값 삭제
+        const sourceValue = sourceBoard.splice(source.index, 1)[0];
+        // 2. 드래그 끝나는 위치에 시작한 값 추가
+        destinationBoard.splice(destination?.index, 0, sourceValue);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
+      });
+    }
   };
-  const onHourChange = (event: TInputEvent) => {
-    setHour(+event.currentTarget.value);
+  const onDragStart = (info: DragStart): void => {
+    setdraggingBoard(info.source.droppableId);
   };
-  const onSecondChange = (event: TInputEvent) => {
-    setSecond(+event.currentTarget.value);
-  };
+
+  useEffect(() => {
+    if (localStorage.getItem("todos") !== null) {
+      setToDo(JSON.parse(localStorage.getItem("todos") as string));
+    } else {
+      setToDo({
+        todo: [],
+        doing: [],
+        done: [],
+      });
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(toDo));
+    return () => {};
+  }, [toDo]);
+
   return (
-    <FormLayout>
-      <Row>
-        <label htmlFor="hour">hour: </label>
-        <input
-          type="text"
-          id="hour"
-          value={hour}
-          onChange={onHourChange}
-          onFocus={() => {
-            setHour(0);
-          }}
-        />
-      </Row>
-      <Row>
-        <label htmlFor="minute">minute: </label>
-        <input
-          type="text"
-          id="minute"
-          value={minute}
-          onChange={onMinuteChange}
-          onFocus={() => {
-            setMinute(0);
-          }}
-        />
-      </Row>
-      <Row>
-        <label htmlFor="second">second: </label>
-        <input
-          type="text"
-          id="second"
-          value={second}
-          onChange={onSecondChange}
-        />
-      </Row>
-    </FormLayout>
+    <PageWrapper>
+      <Wrapper>
+        <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
+          <Droppable
+            direction="horizontal"
+            droppableId="all"
+            isDropDisabled={draggingBoard !== "all"}
+          >
+            {(magic) => (
+              <Boards ref={magic.innerRef} {...magic.droppableProps}>
+                {Object.keys(toDo).map((key, index) => (
+                  <Draggable
+                    draggableId={`board-${key}`}
+                    index={index}
+                    key={`board-${key}`}
+                  >
+                    {(magic, snapshot) => (
+                      <Board
+                        draggableProvider={magic}
+                        todos={toDo[key]}
+                        droppableId={key}
+                      />
+                    )}
+                  </Draggable>
+                ))}
+                {magic.placeholder}
+              </Boards>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </Wrapper>
+    </PageWrapper>
   );
 }
 
-const FormLayout = styled.div`
+const PageWrapper = styled.div`
+  min-height: 100vh;
+  background-color: ${(props) => "#B0A695"};
   display: flex;
   flex-direction: column;
-  gap: 5px;
+  align-items: center;
+  padding-top: 100px;
+  gap: 50px;
 `;
-const Row = styled.div`
+const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+const Boards = styled.div`
+  display: flex;
   gap: 10px;
-  label {
-    flex-basis: 70px;
-    flex-shrink: 0;
-  }
-  input {
-    border: unset;
-    border-bottom: 1px solid #fff;
-    padding: 5px;
-    transition: 0.15s;
-    &:focus {
-      outline: none;
-      border-bottom: 1px solid black;
-    }
-  }
 `;
