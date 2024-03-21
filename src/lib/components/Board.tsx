c09d3e2ed47a16f928d5c56b0d5b3f8fbaad5e27
Hyeongyun <shg7271@gmail.com>
2024-03-21 14:27:54 +0900

    Update: can change board name

--- src/lib/components/Board.tsx
+++ src/lib/components/Board.tsx
@@ -14,10 +14,13 @@ function Board({ todos, droppableId, draggableProvider }: IDraggableCard) {
   const draggingBoard = useRecoilValue(draggingBoardState);
   const setToDo = useSetRecoilState(toDoState);
   const [inputTodo, setInputTodo] = useState("");
+  const [boardName, setBoardName] = useState(droppableId);
+  const [isEditBoardName, setIsEditBoardName] = useState(false);
 
   const onChangeInput = (e: React.FormEvent<HTMLInputElement>): void => {
     setInputTodo(e.currentTarget.value);
   };
+  // 보드의 카드 추가
   const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     if (!inputTodo) return;
@@ -29,12 +32,47 @@ function Board({ todos, droppableId, draggableProvider }: IDraggableCard) {
     });
     setInputTodo("");
   };
+
+  // 보드 이름 수정
+  const onSubmitBoardName = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!boardName) return;
+    setToDo((prev) => {
+      const temp = { ...prev };
+
+      temp[boardName] = [...temp[droppableId]];
+      delete temp[droppableId];
+      return temp;
+    });
+    setInputTodo("");
+  };
   return (
     <Wrapper
       ref={draggableProvider.innerRef}
       {...draggableProvider.draggableProps}
     >
-      <h2 {...draggableProvider.dragHandleProps}>{droppableId}</h2>
+      <form onSubmit={onSubmitBoardName}>
+        {isEditBoardName ? (
+          <input
+            type="text"
+            value={boardName}
+            onChange={(e) => setBoardName(e.target.value)}
+            onBlur={() => {
+              setIsEditBoardName(false);
+            }}
+            autoFocus
+          />
+        ) : (
+          <h2
+            {...draggableProvider.dragHandleProps}
+            onClick={() => {
+              setIsEditBoardName(true);
+            }}
+          >
+            {boardName}
+          </h2>
+        )}
+      </form>
       <form onSubmit={onSubmit}>
         <AddTodoInput
           placeholder={`input ${droppableId} and press enter!`}
